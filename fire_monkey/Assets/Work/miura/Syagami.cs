90fe4efbe81831b54f9d4c3262b6480b566be23c
DA\nwuser <kou.karu.ramu10@icloud.com>
2018-06-08 14:25:38 +0900

    更新太郎丸

---
+++ fire_monkey/Assets/Work/miura/Syagami.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Syagami : MonoBehaviour {
+    SteamVR_Controller.Device head = null;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SteamVR_Utils.RigidTransform Head_tr = head.transform;
+        Debug.Log(Head_tr);
+
+    }
+}
