f8eeaf270dc08f6ee09880d9f7d656f2f8da5eea
DA\nwuser <kou.karu.ramu10@icloud.com>
2018-06-22 10:34:59 +0900

    煙設置、ソース試作

--- fire_monkey/Assets/Work/miura/Syagami.cs
+++ fire_monkey/Assets/Work/miura/Syagami.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
 
-public class Syagami : MonoBehaviour {
-    SteamVR_Controller.Device head = null;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        SteamVR_Utils.RigidTransform Head_tr = head.transform;
-        Debug.Log(Head_tr);
 
+public class Syagami : MonoBehaviour
+{
+    bool ONE;       //1回だけ処理のフラグ
+    int cnt;
+    Vector3 startPos;   //初期位置の代入する変数
+    Vector3 pos;        //常に更新する座標
+    // Use this for initialization
+    void Start()
+    {
+        ONE = true;
+        cnt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ONE)
+        {
+            startPos = transform.position;
+            ONE = false;
+
+        }
+        pos = transform.position;
+
+        if (pos == startPos / 3  )
+        {
+            Debug.Log(pos);
+        }
+        Debug.Log("startPos"+startPos);
+        Debug.Log(pos);
     }
 }
