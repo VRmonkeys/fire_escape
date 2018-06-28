61080333355a001c8fb3749fefce81e6e0dbedde
User Name <sigunaty@gmail.com>
2018-06-28 14:52:00 +0900

    バックドラフトのドアの追加(ゲームオーバー処理未実装)

---
+++ fire_monkey/Assets/work/abe/Script/BackDraftDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackDraftDoor : MonoBehaviour {
+
+    private ParticleSystem particle;
+
+    void Start()
+    {
+        particle = this.GetComponent<ParticleSystem>();
+
+        // ここで Particle System を停止する.
+        particle.Stop();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        // ここで Particle System を開始します.
+        particle.Play();
+    }
+}
