7bfe795725c324de29d3097bbfe65a9cecfd6d0e
DA\nwuser <goroumakisimamu810@gmail.com>
2018-07-12 13:44:03 +0900

    デバッグキー追加、ワープのプレハブ追加

---
+++ fire_monkey/Assets/work/satou/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SCENES
+{
+    TITLE = 0,
+    TUTORIAL,
+    F6,
+    F5,
+    F1,
+    CLEAR,
+    GAMEOVER
+}
+
+static public class SceneManage
+{
+
+    static public void SceneMove(SCENES NextScene)
+    {
+
+        SceneManager.LoadScene((int)NextScene);
+
+    }
+}
---
+++ fire_monkey/Assets/work/satou/Scripts/SceneMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{ 
+    [SerializeField]
+    SCENES scene;
+
+    [SerializeField]
+    SCENES debug;
+
+    //現在のシーンの名前
+    string SceneName;
+
+    //カウント
+    public float time;
+
+    //ワープまでの時間
+    [SerializeField]
+    private float warpTime = 10;
+
+    void Start()
+    {
+        SceneName = SceneManager.GetActiveScene().name;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        DebugKey();
+    }
+
+    void Move()
+    {
+        if(SceneName == "GameOver" || SceneName == "Clear")
+        {
+            time += Time.deltaTime;
+
+            if (time > warpTime)
+            {
+                SceneManage.SceneMove(scene);
+            }
+        }
+    }
+
+    void DebugKey()
+    {
+        //タイトルに戻る
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title");
+        }
+
+        //各シーンの初期位置にワープ
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManage.SceneMove(debug);
+        }
+    }
+}
